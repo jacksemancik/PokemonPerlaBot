429eb723efd9536748b2fe4f6bd32e239d337c64
Jack Semancik <jack.semancik@yahoo.com>
2017-08-11 17:26:08 +0900

    add argument sanitisation class

---
+++ sanitise.py
@@ -0,0 +1,34 @@
+class Sanitise(object):
+	def gender(args):
+		arb = ''
+		for each in args:
+			arb += each
+		arb = arb.lower()
+		if arb == 'male' or arb == 'boy' or arb =='man':
+			return 'Male'
+		elif arb == 'female' or arb == 'girl' or arb == 'woman':
+			return 'Female'
+		else:
+			raise ValueError
+	
+	def game(args):
+		arb = ''
+		for each in args:
+			arb += each
+		arb = arb.lower()
+		if arb == 'sapphire':
+			return 'Sapphire'
+		elif arb == 'ruby':
+			return 'Ruby'
+		elif arb == 'emerald':
+			return 'Emerald'
+		elif arb == 'perla':
+			return 'Perla'
+		elif arb == 'firered' or arb == 'fire':
+			return 'Fire'
+		elif arb == 'leafgreen' or arb == 'leaf':
+			return 'Leaf'
+		elif arb == 'chaosblack' or arb == 'chaos':
+			return 'Chaos'
+		else:
+			raise ValueError
